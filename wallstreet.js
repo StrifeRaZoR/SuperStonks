cb6a271fcf5089f61564a700b410370f0edab540
StrifeRaZoR <97868924+StrifeRaZoR@users.noreply.github.com>
2022-01-20 15:12:37 -0500

    Update wallstreet.js
    ACTIVE TRADING!

--- wallstreet.js
+++ wallstreet.js
@@ -5,8 +5,13 @@ import { getGlobal, html, Mitt, setGlobal } from '/bb-vue/lib.js'
 /** @param { import("~/ns").NS } ns */
 export async function main(ns) {
 var ticker = ns.args[0];
+ns.tail();
+
   ns.run("wallstreet-data.js", 1, ticker)
   try {
+    ns.disableLog('disableLog');
+    ns.disableLog('asleep');
+    ns.disableLog('sleep');
     ns.tprint("Chart Loading.  Please click CLOSE CHART before loading a new chart.")
     await new AppFactory(ns).mount({
       config: { id: 'svg-chart-app-wallstreet', showTips: false },
@@ -20,22 +25,49 @@ var ticker = ns.args[0];
   // Listen for specific event
 
   let wantsShutdown = false
+  let buyMaxLongShares = false
+  let buyMaxShortShares = false
+  let sellAllShares = false
   let bus = Mitt().createBus()
   bus.on('wantsShutdown', () => (wantsShutdown = true))
+  bus.on('buyMaxLong', () => (buyMaxLongShares = true))
+  bus.on('buyMaxShort', () => (buyMaxShortShares = true))
+  bus.on('closeAllPositions', () => (sellAllShares = true))
   setGlobal('tickerBus', bus)
 
   // Instead of closing, let's keep this running
   while (wantsShutdown == false) {
+    var position = ns.stock.getPosition(ticker);
+      if (buyMaxLongShares == true) {
+
+        ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 10000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)))
+        buyMaxLongShares = false;
+
+      }
+      if (buyMaxShortShares == true) {
+
+        ns.stock.short(ticker, Math.min((ns.getServerMoneyAvailable("home") - 10000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)))
+        buyMaxShortShares = false;
+
+      }
+      if (sellAllShares == true) {
+
+        ns.stock.sell(ticker, position[0]);
+        ns.stock.sellShort(ticker, position[2]);
+        sellAllShares = false;
+
+      }
+
    await ns.asleep(500)
   }
-
+//ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 1000000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker))))
 //ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 1000000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)))
 }
 
 export const SVGChartContainerwallstreet = 'svgChartContainerwallstreet'
 const ChartContainerwallstreet = {
   name: 'svg-chart-wallstreet',
-  inject: ['appShutdown', 'sendOrderLong'], 
+  inject: ['appShutdown',], 
   template: html`
     <bbv-win
       class="__CMP_NAME__"
@@ -47,6 +79,9 @@ const ChartContainerwallstreet = {
       <div v-once id="${SVGChartContainerwallstreet}" />
       <template #actions>
         <bbv-button @click="shutdownAll">Close Chart</bbv-button>
+        <div><bbv-button @click="buyMaxLong">BUY MAX [LONG]</bbv-button></div>
+        <div><bbv-button @click="buyMaxShort">BUY MAX [SHORT]</bbv-button></div>
+        <div><bbv-button @click="closeAllPositions">SELL/CLOSE ALL</bbv-button></div>
       </template>
     </bbv-win>
   `,
@@ -59,6 +94,15 @@ const ChartContainerwallstreet = {
     shutdownAll() {
       getGlobal('tickerBus').emit('wantsShutdown')
       this.appShutdown() 
+    },
+    buyMaxLong() {
+      getGlobal('tickerBus').emit('buyMaxLong')
+    },
+    buyMaxShort() {
+      getGlobal('tickerBus').emit('buyMaxShort')
+    },
+    closeAllPositions() {
+      getGlobal('tickerBus').emit('closeAllPositions')
     },
   }, 
 }
