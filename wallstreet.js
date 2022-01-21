40b29a276236c13a6609bdc96ef5a96cc4691e00
StrifeRaZoR <97868924+StrifeRaZoR@users.noreply.github.com>
2022-01-20 20:57:52 -0500

    Update wallstreet.js
    ACTIVE TRADING!  WOOO! PROFIT REPORTING!  DOUBLE WOOO!

--- wallstreet.js
+++ wallstreet.js
@@ -4,8 +4,7 @@ import { getGlobal, html, Mitt, setGlobal } from '/bb-vue/lib.js'
 
 /** @param { import("~/ns").NS } ns */
 export async function main(ns) {
-var ticker = ns.args[0];
-ns.tail();
+  var ticker = ns.args[0];
 
   ns.run("wallstreet-data.js", 1, ticker)
   try {
@@ -22,10 +21,12 @@ ns.tail();
     ns.tprint(error.toString())
     ns.exit()
   }
-  // Listen for specific event
 
+  // Listen for specific event
   let wantsShutdown = false
   let buyMaxLongShares = false
+  let closeLongPosition = false
+  let closeShortPosition = false
   let buyMaxShortShares = false
   let sellAllShares = false
   let bus = Mitt().createBus()
@@ -33,47 +34,100 @@ ns.tail();
   bus.on('buyMaxLong', () => (buyMaxLongShares = true))
   bus.on('buyMaxShort', () => (buyMaxShortShares = true))
   bus.on('closeAllPositions', () => (sellAllShares = true))
+  bus.on('closeLong', () => (closeLongPosition = true))
+  bus.on('closeShort', () => (closeShortPosition = true))
   setGlobal('tickerBus', bus)
 
   // Instead of closing, let's keep this running
   while (wantsShutdown == false) {
-    var position = ns.stock.getPosition(ticker);
-      if (buyMaxLongShares == true) {
+    //Let's go ahead and render the profit monitor into the stats overview hook.
+    const doc = eval('document'); // This is expensive! (25GB RAM) Perhaps there's a way around it? ;)
+    const hook0 = doc.getElementById('overview-extra-hook-0');
+    const hook1 = doc.getElementById('overview-extra-hook-1');
+    var ticker = ns.args[0];
+    var position = ns.stock.getPosition(ns.sprintf(ticker));
 
-        ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 10000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)))
-        buyMaxLongShares = false;
+    try {
+      const headers = []
+      const values = [];
+      // Current stock LONG position profits
+      headers.push("[LONG] Profit");
+      values.push(ns.nFormat(ns.stock.getSaleGain(ticker, position[0], "Long") - (position[0] * position[1]), '0.00a'));
+      // Current stock SHORT position profits
+      headers.push("[SHORT] Profit");
+      values.push(ns.nFormat(ns.stock.getSaleGain(ticker, position[2], "Short") - (position[2] * position[3]), '0.00a'));
+      // TODO: Add more neat stuff
 
-      }
-      if (buyMaxShortShares == true) {
+      // Now drop it into the placeholder elements
+      hook0.innerText = headers.join(" \n");
+      hook1.innerText = values.join("\n");
 
-        ns.stock.short(ticker, Math.min((ns.getServerMoneyAvailable("home") - 10000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)))
-        buyMaxShortShares = false;
+    } catch (err) { // This might come in handy later
 
-      }
-      if (sellAllShares == true) {
+      ns.print("ERROR: Update Skipped: " + String(err));
 
-        ns.stock.sell(ticker, position[0]);
-        ns.stock.sellShort(ticker, position[2]);
-        sellAllShares = false;
+    }
 
-      }
+    await ns.asleep(200);
 
-   await ns.asleep(500)
+    var ticker = ns.args[0];
+    var position = ns.stock.getPosition(ns.sprintf(ticker));
+
+    if (buyMaxLongShares == true) {
+      var position = ns.stock.getPosition(ns.sprintf(ticker));
+      ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 1000000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)));
+      await ns.asleep(100);
+      ns.toast("LONG ORDER PLACED - " + ticker + ".", "info", 10000);
+      buyMaxLongShares = false;
+
+    }
+    if (buyMaxShortShares == true) {
+      var position = ns.stock.getPosition(ns.sprintf(ticker));
+      ns.stock.short(ticker, Math.min((ns.getServerMoneyAvailable("home") - 1000000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)));
+      await ns.asleep(100);
+      ns.toast("SHORT ORDER PLACED - " + ticker + ".", "info", 10000);
+      buyMaxShortShares = false;
+
+    }
+    if (sellAllShares == true) {
+      var position = ns.stock.getPosition(ns.sprintf(ticker));
+      ns.stock.sell(ticker, position[0]);
+      ns.stock.sellShort(ticker, position[2]);
+      ns.toast("CLOSED **ALL** POSITIONS FOR " + ticker, "warning", 5000);
+      sellAllShares = false;
+
+    }
+    if (closeLongPosition == true) {
+      var position = ns.stock.getPosition(ns.sprintf(ticker));
+      ns.stock.sell(ticker, position[0]);
+      ns.toast("CLOSED LONG POSITION FOR " + ticker, "warning", 5000);
+      closeLongPosition = false;
+
+    }
+    if (closeShortPosition == true) {
+      var position = ns.stock.getPosition(ns.sprintf(ticker));
+      ns.stock.sellShort(ticker, position[2]);
+      ns.toast("CLOSED SHORT POSITION FOR " + ticker, "warning", 5000);
+      closeShortPosition = false;
+
+    }
+
+    await ns.asleep(500)
+
   }
-//ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 1000000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker))))
-//ns.stock.buy(ticker, Math.min((ns.getServerMoneyAvailable("home") - 1000000) / ns.stock.getAskPrice(ticker), ns.stock.getMaxShares(ticker)))
+
 }
 
 export const SVGChartContainerwallstreet = 'svgChartContainerwallstreet'
 const ChartContainerwallstreet = {
   name: 'svg-chart-wallstreet',
-  inject: ['appShutdown',], 
+  inject: ['appShutdown',],
   template: html`
     <bbv-win
       class="__CMP_NAME__"
       title="Wall Street Wave Trend"
       no-pad
-      start-height="60%"
+      start-height="70%"
       start-width="40%"
     >
       <div v-once id="${SVGChartContainerwallstreet}" />
@@ -81,7 +135,9 @@ const ChartContainerwallstreet = {
         <bbv-button @click="shutdownAll">Close Chart</bbv-button>
         <div><bbv-button @click="buyMaxLong">BUY MAX [LONG]</bbv-button></div>
         <div><bbv-button @click="buyMaxShort">BUY MAX [SHORT]</bbv-button></div>
-        <div><bbv-button @click="closeAllPositions">SELL/CLOSE ALL</bbv-button></div>
+        <div><bbv-button @click="sellMaxLong">SELL MAX [LONG]</bbv-button></div>
+        <div><bbv-button @click="sellMaxShort">SELL MAX [SHORT]</bbv-button></div>
+        <div><bbv-button @click="closeAllPositions">CLOSE ALL [LONG/SHORT]</bbv-button></div>
       </template>
     </bbv-win>
   `,
@@ -93,7 +149,7 @@ const ChartContainerwallstreet = {
   methods: {
     shutdownAll() {
       getGlobal('tickerBus').emit('wantsShutdown')
-      this.appShutdown() 
+      this.appShutdown()
     },
     buyMaxLong() {
       getGlobal('tickerBus').emit('buyMaxLong')
@@ -104,5 +160,11 @@ const ChartContainerwallstreet = {
     closeAllPositions() {
       getGlobal('tickerBus').emit('closeAllPositions')
     },
-  }, 
+    sellMaxLong() {
+      getGlobal('tickerBus').emit('closeLong')
+    },
+    sellMaxShort() {
+      getGlobal('tickerBus').emit('closeShort')
+    },
+  },
 }
